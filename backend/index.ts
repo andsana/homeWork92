e077b514d708d92001c921c5cea5fb1f972aead3
Susanna Andreeva <sanadex90@gmail.com>
2024-03-18 15:44:36 +0900

    add websocket chat functionality and username handling

---
+++ backend/index.ts
@@ -0,0 +1,57 @@
+import express from 'express';
+import expressWs from 'express-ws';
+import cors from 'cors';
+import {ActiveConnections, IncomingMessage} from './types';
+import crypto from 'crypto';
+
+const app = express();
+expressWs(app);
+
+const port = 8000;
+app.use(cors());
+const router = express.Router();
+
+const activeConnections: ActiveConnections = {};
+
+router.ws('/chat', (ws, req) => {
+  const id = crypto.randomUUID();
+  console.log('client connected! id=', id);
+  activeConnections[id] = ws;
+
+  let username = 'Anonymous';
+
+  ws.on('message', (msg) => {
+    const decodedMessage: IncomingMessage = JSON.parse(msg.toString());
+
+    switch (decodedMessage.type) {
+      case 'SET_USERNAME':
+        username = decodedMessage.payload;
+        break;
+      case 'SEND_MESSAGE':
+        Object.keys(activeConnections).forEach(connId => {
+          const conn = activeConnections[connId];
+          conn.send(JSON.stringify({
+            type: 'NEW_MESSAGE',
+            payload: {
+              username,
+              text: decodedMessage.payload
+            }
+          }));
+        });
+        break;
+      default:
+        console.log('Unknown message type:', decodedMessage.type);
+    }
+  });
+
+  ws.on('close', () => {
+    console.log('client disconnected! id=', id);
+    delete activeConnections[id];
+  });
+});
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server started on ${port} port!`);
+});
